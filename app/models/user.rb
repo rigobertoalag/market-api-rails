2836dce49d7b78c2bdf21184c3b177e1da6e042e
rigobertoalag <a20150338@utem.edu.mx>
2021-12-17 10:31:04 -0600

    User Roles implements

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  enum role: [:user, :admin]
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 
   has_many :items
 
